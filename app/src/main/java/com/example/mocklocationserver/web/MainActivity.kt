43629f4b9600cd3fc9283689fff9adb80780235c
chiq <chi.qiang@ip-consulting.co.jp>
2025-01-22 14:04:34 +0800

    main显示ip

--- app/src/main/java/com/example/mocklocationserver/web/MainActivity.kt
+++ app/src/main/java/com/example/mocklocationserver/web/MainActivity.kt
@@ -1,25 +1,37 @@
 package com.example.mocklocationserver.web
 
 import android.Manifest
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mocklocationserver.web.databinding.ActivityMainBinding
 import com.example.mocklocationserver.web.service.MockLocationService
+import com.example.mocklocationserver.web.service.WifiInfoCollectService
+import kotlinx.coroutines.flow.filterNotNull
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String?>
 
     private var alertDialog: AlertDialog? = null
+
+    val apiKey = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +40,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.startService.setOnClickListener { startWebServer() }
         binding.stopService.setOnClickListener { stopWebServer() }
+
+
+        // 获取当前设备的 IP 地址
+        val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val ip = wifiManager.connectionInfo.ipAddress
+
+        // 使用位移操作分离 IP 地址的各个字节
+        val i1 = ip and 0xff
+        val i2 = (ip ushr 8) and 0xff
+        val i3 = (ip ushr 16) and 0xff
+        val i4 = (ip ushr 24) and 0xff
+
+        // 格式化并打印 IP 地址
+        val ipAddress = "$i1.$i2.$i3.$i4"
+        binding.ipTextView.text="$ipAddress:8080"
+
+        // 长按复制 IP 地址和 URL 到粘贴板
+        binding.ipTextView.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val textToCopy = "$ipAddress:8080/?key=$apiKey"
+            val clip = ClipData.newPlainText("IP Address", textToCopy)
+            clipboard.setPrimaryClip(clip)
+
+            // 显示提示消息（可选）
+            Toast.makeText(this, "IP 地址和 URL 已复制到剪贴板", Toast.LENGTH_SHORT).show()
+        }
+
 
         requestPermissionLauncher =
             registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
@@ -93,4 +132,6 @@ class MainActivity : AppCompatActivity() {
         val i = Intent(this, MockLocationService::class.java)
         stopService(i)
     }
+
+
 }
